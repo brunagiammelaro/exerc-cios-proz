559696365f70c79cef280a457945742d82402499
brunagiammelaro <138893476+brunagiammelaro@users.noreply.github.com>
2023-10-30 19:26:34 -0300

    exercício templateString

---
+++ templateString/template.js
@@ -0,0 +1,14 @@
+function operacoesBasicas(num1, num2) {
+    const soma = `Soma ${num1} + ${num2} = ${num1 + num2}`;
+    const subtracao = `Subtração ${num1} - ${num2} = ${num1 - num2}`;
+    const multiplicacao = `Multiplicação ${num1} x ${num2} = ${num1 * num2}`;
+    const divisao = `Divisão ${num1} / ${num2} = ${num1 / num2}`;
+    
+    console.log(soma);
+    console.log(subtracao);
+    console.log(multiplicacao);
+    console.log(divisao);
+  }
+  
+ 
+  operacoesBasicas(4, 5);
